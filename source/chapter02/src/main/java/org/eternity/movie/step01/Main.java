8c9170464c71a43f23d8d90998d9a7c7fa9eafae
aiden.sim <aiden.sim@kakaocorp.com>
2021-01-18 02:30:54 +0900

    chater02 step01

---
+++ source/chapter02/src/main/java/org/eternity/movie/step01/Main.java
@@ -0,0 +1,33 @@
+package org.eternity.movie.step01;
+
+import java.time.DayOfWeek;
+import java.time.Duration;
+import java.time.LocalTime;
+
+public class Main {
+    public static void main(String[] args) {
+        Movie avater = new Movie("아바타",
+                                 Duration.ofMinutes(120),
+                                 Money.wons(10000),
+                                 new AmountDiscountPolicy(Money.wons(800),
+                                                          new SequenceCondition(1),
+                                                          new SequenceCondition(10),
+                                                          new PeriodCondition(DayOfWeek.MONDAY, LocalTime.of(10, 0),
+                                                                              LocalTime.of(11, 59)),
+                                                          new PeriodCondition(DayOfWeek.THURSDAY,
+                                                                              LocalTime.of(18, 0), LocalTime.of(21, 0))
+                                 )
+        );
+
+        Movie titanic = new Movie("타이타닉",
+                                  Duration.ofMinutes(180),
+                                  Money.wons(11000),
+                                  new PercentDiscountPolicy(0.1,
+                                                            new PeriodCondition(DayOfWeek.WEDNESDAY,
+                                                                                LocalTime.of(14, 0),
+                                                                                LocalTime.of(17, 0)),
+                                                            new SequenceCondition(2),
+                                                            new PeriodCondition(DayOfWeek.THURSDAY, LocalTime.of(10, 0),
+                                                                                LocalTime.of(14, 0))));
+    }
+}
